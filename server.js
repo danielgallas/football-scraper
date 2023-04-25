f8820f1bafe34dc1949142541d6d0367b0c9ac4d
Daniel Gallas <danielgallas2@gmail.com>
2023-04-25 16:02:22 +0100

    scrapes scores from ESPN site

--- server.js
+++ server.js
@@ -24,24 +24,24 @@ async function getScores() {
   //   return data;
   // });
 
-  await page.goto(
-    "https://www.whoscored.com/Teams/1244/Fixtures/Brazil-Gremio",
-    {
-      waitUntil: "networkidle0",
-    }
-  );
+  // await page.goto(
+  //   "https://www.whoscored.com/Teams/1244/Fixtures/Brazil-Gremio",
+  //   {
+  //     waitUntil: "networkidle0",
+  //   }
+  // );
 
-  await page.click("button.css-1wc0q5e");
-  await page.select("#team-fixture-tournaments", "Brasileirão");
+  // await page.click("button.css-1wc0q5e");
+  // await page.select("#team-fixture-tournaments", "Brasileirão");
 
-  const teams = await page.evaluate(() => {
-    const table = Array.from(
-      document.querySelectorAll(
-        ".col12-lg-12.col12-m-12.col12-s-12.col12-xs-12.item.divtable-row.alt"
-      )
-    );
-    return table;
-  });
+  // const teams = await page.evaluate(() => {
+  //   const table = Array.from(
+  //     document.querySelectorAll(
+  //       ".col12-lg-12.col12-m-12.col12-s-12.col12-xs-12.item.divtable-row.alt"
+  //     )
+  //   );
+  //   return table;
+  // });
 
   // const data = table.map((item) => ({
   //   title: item.querySelector(
@@ -49,7 +49,55 @@ async function getScores() {
   //   ).innerText,
   // }));
 
+  await page.goto(
+    "https://www.espn.co.uk/football/team/results/_/id/6273/league/BRA.1",
+    {
+      waitUntil: "networkidle0",
+    }
+  );
+
+  await page.click("#onetrust-accept-btn-handler");
+  // await page.select("#team-fixture-tournaments", "Brasileirão");
+
+  const teams = await page.evaluate(() => {
+    const scoreRow = Array.from(
+      document.querySelectorAll(".Table__Team.score")
+    );
+    const scoreResultLine = scoreRow.map((item) => {
+      const anchorLink = item.querySelectorAll("a.AnchorLink");
+      const finalResult = anchorLink[1].innerText;
+      return {
+        finalScore1: finalResult.at(0),
+        finalScore2: finalResult.at(-1),
+      };
+    });
+    return scoreResultLine;
+
+    // const table = Array.from(
+    //   document.querySelectorAll(".Table__TR.Table__TR--sm.Table__even")
+    // );
+    // const data = table.map((item) => ({
+    //   team1: item.querySelector(".Table__TD .local.flex.items-center a")
+    //     .innerText,
+    //   team2: item.querySelector(".away.flex.items-center a").innerText,
+    //   // finalScore1: scoreResultLine[item].finalScore1,
+    //   // finalScore2: scoreResultLine[item].finalScore2,
+    // }));
+    // return data;
+  });
+
   console.log(teams);
+
+  //       finalScore1: item
+  //        .querySelectorAll(".Table__Team.score")
+  //      .map((game) => game[1].querySelectorAll("a.AnchorLink").innerText),
+
+  // game = document.querySelectorAll(".Table__Team.score");
+  // score = game[1].querySelectorAll("a.AnchorLink");
+
+  // item
+  //   .querySelectorAll(".Table__Team.score")
+  //   .map((game) => game[1].querySelectorAll("a.AnchorLink").innerText);
 
   // const teams = await page.evaluate(() => {
   //   const table = Array.from(document.querySelectorAll(".product_pod"));
